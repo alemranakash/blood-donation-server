0128918346a9a441e8f475a46492283104da262b
alemranakash <alemranexpert@gmail.com>
2023-11-24 16:31:07 +0600

    Implemented GET method for retrieving user data and PATCH method for updating user information.

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ftdzayi.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -39,6 +39,38 @@ app.post('/users', async (req, res) => {
   const result = await userCollection.insertOne(item);
   res.send(result);
 });
+
+app.get('/users', async (req, res) => {
+  const result = await userCollection.find().toArray();
+  res.send(result);
+});
+
+app.get('/dashboard/userProfile/updateUser/:id', async (req, res) => {
+  const id = req.params.id;
+  const query = { _id: new ObjectId(id) }
+  const result = await userCollection.findOne(query)
+  res.send(result)
+})
+
+
+app.patch('/dashboard/userProfile/updateUser/:id', async (req, res) => {
+  const id = req.params.id;
+  const filter = { _id: new ObjectId(id) }
+  const options = { upsert: true }
+  const updatedUserData = req.body;
+  const user = {
+    $set:{
+      name: updatedUserData.name, 
+      photoUrl: updatedUserData.photoUrl, 
+      bloodGroup: updatedUserData.bloodGroup, 
+      district: updatedUserData.district, 
+      upazila: updatedUserData.upazila
+    }
+  }
+  const result = await userCollection.updateOne(filter, user, options)
+  res.send(result)
+})
+
 
 
 
